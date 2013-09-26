9cba63b9d2413c1f912160eeaa08f083e0fd08cd
Lei Wang <tendant@gmail.com>
2013-09-26 13:15:02 -0700

    update test page.

--- project.clj
+++ project.clj
@@ -5,7 +5,14 @@
                  [compojure "1.1.5"]
                  [aleph "0.3.0"]
                  [lamina "0.5.0"]
-                 [ring/ring-core "1.2.0"]]
+                 [ring/ring-core "1.1.8"]
+                 [clj-time "0.4.4"]
+                 [commons-fileupload "1.3"]
+                 [commons-io "2.4"]
+                 [javax.servlet/servlet-api "2.5"]
+                 [org.clojure/tools.reader "0.7.3"]
+                 [ring/ring-codec "1.0.0"]
+                 ]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler camera2.handler/app}
   :main camera2.handler
