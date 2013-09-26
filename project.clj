2873fd5de106613828edabdfa4dc848547b777e0
Lei Wang <tendant@gmail.com>
2013-09-26 02:37:35 -0700

    still working.

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
                  [aleph "0.3.0"]
                  [lamina "0.5.0"]
                  [ring/ring-devel "1.2.0"]
+                 [ring/ring-httpcore-adapter "0.3.5"]
+                 [ring/ring-jetty-adapter "0.3.5"]
                  ]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler camera2.handler/app}
